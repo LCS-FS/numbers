78335fc1825c91cd4112f3a044822af49f411242
Lucas Sousa <lcsferreiradesousa@hotmail.com>
2021-12-01 15:33:01 +0000

    PositiveFilterTest.groovy implemented

--- src/test/groovy/PositiveFilterTest.groovy
+++ src/test/groovy/PositiveFilterTest.groovy
@@ -1,2 +1,20 @@
-class PositiveFilterTest {
+import com.aor.numbers.PositiveFilter
+import spock.lang.Specification
+
+class PositiveFilterTest extends Specification{
+    private int number1, number2, number3
+
+    def "setup"(){
+        number1 = 10
+        number2 = -10
+        number3 = 0
+    }
+
+    def "pos filter test groovy"(){
+        PositiveFilter positiveFilter = new PositiveFilter()
+        expect:
+            positiveFilter.accept(number1) == true
+            positiveFilter.accept(number2) == false
+            positiveFilter.accept(number3) == false
+    }
 }
