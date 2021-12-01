89176ebce562e0445b2d9fb587154660e5a77183
Lucas Sousa <lcsferreiradesousa@hotmail.com>
2021-12-01 15:28:13 +0000

    ListSorterTest.groovy implemented

--- src/test/groovy/ListSorterTest.groovy
+++ src/test/groovy/ListSorterTest.groovy
@@ -1,2 +1,17 @@
-class ListSorterTest {
+import com.aor.numbers.ListSorter
+import spock.lang.Specification
+
+class ListSorterTest extends Specification{
+    private List list, expected
+
+    def "setup"(){
+        list = Arrays.asList(3, 2, 6, 1, 4, 5, 7)
+        expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7)
+    }
+
+    def "sort groovy"(){
+        def sorter = new ListSorter()
+        expect:
+            sorter.sort(list) == expected
+    }
 }
